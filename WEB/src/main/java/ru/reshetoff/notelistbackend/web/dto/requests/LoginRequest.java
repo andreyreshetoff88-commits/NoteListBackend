419c4e036b5da6e3de6474998f33c94b45c2b261
andreyreshetoff88-commits <andrey.reshetoff.88@gmail.com>
2026-05-31 21:19:42 +0300

    убрал лишнюю валидацию полей для LoginRequest исправил ответ если срок токена истек

--- WEB/src/main/java/ru/reshetoff/notelistbackend/web/dto/requests/LoginRequest.java
+++ WEB/src/main/java/ru/reshetoff/notelistbackend/web/dto/requests/LoginRequest.java
@@ -1,22 +1,13 @@
 package ru.reshetoff.notelistbackend.web.dto.requests;
 
-import io.swagger.v3.oas.annotations.media.Schema;
-import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import lombok.Data;
 
 @Data
 public class LoginRequest {
     @NotBlank(message = "Email is required")
-    @Email(message = "Email should be valid")
     private String email;
 
     @NotBlank(message = "Password is required")
-    @Schema(description = "Password must be at least 8 characters, contain uppercase, lowercase, digit and special character")
-    @Pattern(
-            regexp = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%^&+=!_])(?=\\S+$).{8,}$",
-            message = "Password must be at least 8 characters, contain uppercase, lowercase, digit and special character"
-    )
     private String password;
 }
--- WEB/src/main/java/ru/reshetoff/notelistbackend/web/security/JwtAuthenticationFilter.java
+++ WEB/src/main/java/ru/reshetoff/notelistbackend/web/security/JwtAuthenticationFilter.java
@@ -1,11 +1,14 @@
 package ru.reshetoff.notelistbackend.web.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.jsonwebtoken.ExpiredJwtException;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.jspecify.annotations.NonNull;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +16,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.WebAuthenticationDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import ru.reshetoff.notelistbackend.web.dto.response.ErrorResponse;
 
 import java.io.IOException;
 
@@ -21,6 +25,7 @@ import java.io.IOException;
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
+    private final ObjectMapper objectMapper;
 
     @Override
     protected void doFilterInternal(
@@ -36,17 +41,31 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
 
         final String token = authHeader.replace("Bearer ", "");
-        final String userEmail = jwtService.extractEmail(token);
-        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-            UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
-            if (jwtService.isTokenValid(token, userDetails)) {
-                UsernamePasswordAuthenticationToken authToken =
-                        new UsernamePasswordAuthenticationToken(
-                                userDetails, null, userDetails.getAuthorities()
-                        );
-                authToken.setDetails(new WebAuthenticationDetails(request));
-                SecurityContextHolder.getContext().setAuthentication(authToken);
+        try {
+            final String userEmail = jwtService.extractEmail(token);
+            if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+                UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
+                if (jwtService.isTokenValid(token, userDetails)) {
+                    UsernamePasswordAuthenticationToken authToken =
+                            new UsernamePasswordAuthenticationToken(
+                                    userDetails, null, userDetails.getAuthorities()
+                            );
+                    authToken.setDetails(new WebAuthenticationDetails(request));
+                    SecurityContextHolder.getContext().setAuthentication(authToken);
+                }
             }
+        }catch (ExpiredJwtException e){
+            response.setStatus(HttpStatus.UNAUTHORIZED.value());
+            response.setContentType("application/json");
+            response.setCharacterEncoding("UTF-8");
+
+            ErrorResponse errorResponse = new ErrorResponse(
+                    "TOKEN_EXPIRED",
+                    "error",
+                    "Access token expired"
+            );
+            response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+            return;
         }
 
         filterChain.doFilter(request, response);
